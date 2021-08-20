cb32979f7f72ebd6f15a79dc82b3a980085f25ff
PJ <wooritobi@gmail.com>
2021-08-21 01:07:11 +0900

    apollo.js modified for production

--- src/apollo.js
+++ src/apollo.js
@@ -1,14 +1,32 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+import { setContext } from "@apollo/client/link/context";
 
 const TOKEN = "token";
 
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
 export const darkModeVar = makeVar(false);
 
+const httpLink = createUploadLink({
+    uri: process.env.MODE_ENV === "production" 
+    ? "https://nomadcoffee-bk.herokuapp.com/graphql"
+    : "http://localhost:4000/graphql",
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(TOKEN);
+    return {
+      headers: {
+        ...headers,
+        ...(token && { token }),
+      },
+    };
+  });
+
 export const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-});
+  });
 
 export const logUserIn = (token) => {
     localStorage.setItem(TOKEN, token);
